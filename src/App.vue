55e1e1e17400b0b8b1c18e9553a7825a8b1db927
Viktor Kratz <35398895+Krattanovich@users.noreply.github.com>
2020-10-27 22:01:31 +0100

    Header added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template >
     <div id="app" >
         
+        <HeaderTop msg="Header text" />
         <Home msg="Hello world!" />
-
     </div>
 
 
 </template>
 
 <script>
+    import Header from './components/Header.vue';
     import Home from './components/Home.vue';
 
     export default {
         name: 'app',
         components: {
+            Header,
             Home
         }
     };
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+﻿<template>
+    <header class="HeaderTop">
+        <h1>{{ headtxt }}</h1>
+    </header>
+</template>
+
+<script>
+
+    export default {
+        name: 'HeaderTop',
+        props: { headtxt: String }
+
+    }
+
+</script>
+
+<style scoped>
+    
+</style>
